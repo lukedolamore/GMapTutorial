6a3b82a4431b052a344dd89fbe97652b65c2a124
lukedolamore <48756098+lukedolamore@users.noreply.github.com>
2019-10-21 22:12:49 +1300

    Tutorial Finished

--- GMapTutorial/Form1.Designer.cs
+++ GMapTutorial/Form1.Designer.cs
@@ -28,13 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gmap = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // gmap
+            // 
+            this.gmap.Bearing = 0F;
+            this.gmap.CanDragMap = true;
+            this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmap.GrayScaleMode = false;
+            this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gmap.LevelsKeepInMemmory = 5;
+            this.gmap.Location = new System.Drawing.Point(12, 12);
+            this.gmap.MarkersEnabled = true;
+            this.gmap.MaxZoom = 18;
+            this.gmap.MinZoom = 2;
+            this.gmap.MouseWheelZoomEnabled = true;
+            this.gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gmap.Name = "gmap";
+            this.gmap.NegativeMode = false;
+            this.gmap.PolygonsEnabled = true;
+            this.gmap.RetryLoadTile = 0;
+            this.gmap.RoutesEnabled = true;
+            this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmap.ShowTileGridLines = false;
+            this.gmap.Size = new System.Drawing.Size(776, 426);
+            this.gmap.TabIndex = 0;
+            this.gmap.Zoom = 13D;
+            this.gmap.OnMarkerClick += new GMap.NET.WindowsForms.MarkerClick(this.gmap_OnMarkerClick);
+            this.gmap.OnPolygonClick += new GMap.NET.WindowsForms.PolygonClick(this.gmap_OnPolygonClick);
+            this.gmap.Load += new System.EventHandler(this.gmap_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gmap);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gmap;
     }
 }
 
--- GMapTutorial/Form1.cs
+++ GMapTutorial/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
 
 namespace GMapTutorial
 {
@@ -15,6 +19,63 @@ namespace GMapTutorial
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void gmap_Load(object sender, EventArgs e)
+        {
+            gmap.MapProvider = GoogleMapProvider.Instance;//Changed to Google maps
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+            //gmap.SetPositionByKeywords("Invercargill, New Zealand");
+            gmap.Position = new PointLatLng(-46.4132, 168.3538);
+            gmap.ShowCenter = false;//get rids of red cross in the middle
+
+            //pg2
+            GMapOverlay markers = new GMapOverlay("markers");
+            GMapMarker marker = new GMarkerGoogle(new PointLatLng(-46.4132, 168.3538),GMarkerGoogleType.blue_pushpin);
+            marker.ToolTipText = "Hello\nout there";//Changing the tooltip
+            marker.ToolTip.Fill = Brushes.Black;
+            marker.ToolTip.Foreground = Brushes.White;
+            marker.ToolTip.Stroke = Pens.Black;
+            marker.ToolTip.TextPadding = new Size(20, 20);
+            marker.ToolTipMode = MarkerTooltipMode.OnMouseOver;//
+            markers.Markers.Add(marker);
+            gmap.Overlays.Add(markers);
+
+            //pg3 Polygons
+            GMapOverlay polygons = new GMapOverlay("polygons");
+            List<PointLatLng> points = new List<PointLatLng>();
+            points.Add(new PointLatLng(-46.413279, 168.353798));
+            points.Add(new PointLatLng(-46.414529, 168.353864));
+            points.Add(new PointLatLng(-46.414492, 168.359946));
+            points.Add(new PointLatLng(-46.413227, 168.359955));           
+            GMapPolygon polygon = new GMapPolygon(points, "Southern Institute of Technology Invercargill");
+            polygon.Fill = new SolidBrush(Color.FromArgb(50, Color.Red));
+            polygon.Stroke = new Pen(Color.Red, 1);
+            polygons.Polygons.Add(polygon);
+            gmap.Overlays.Add(polygons);
+
+            //pg3 Routes
+            GMapOverlay routes = new GMapOverlay("routes");
+            List<PointLatLng> rpoints = new List<PointLatLng>();
+            rpoints.Add(new PointLatLng(-46.413902, 168.355514));
+            rpoints.Add(new PointLatLng(-46.414115, 168.352888));
+            rpoints.Add(new PointLatLng(-46.412349, 168.347600));
+            GMapRoute route = new GMapRoute(rpoints, "A walk to PakNSave and the Movies");
+            route.Stroke = new Pen(Color.Red, 3);
+            Console.WriteLine(route.Distance);//return route distance in KM
+            routes.Routes.Add(route);
+            gmap.Overlays.Add(routes);
+
+        }//gmap_Load
+
+        private void gmap_OnMarkerClick(GMapMarker item, MouseEventArgs e)
+        {
+            Console.WriteLine(string.Format("Marker {0} was clicked.", item.Tag));
+        }
+
+        private void gmap_OnPolygonClick(GMapPolygon item, MouseEventArgs e)
+        {
+            Console.WriteLine(string.Format("Polygon {0} with tag {1} was clicked", item.Name, item.Tag));
         }
     }
 }
